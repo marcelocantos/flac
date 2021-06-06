f7484ebf08b1810c3605ed252bfc18e53a60c779
Marcelo Cantos <marcelo.cantos@gmail.com>
2021-06-06 14:44:05 +1000

    Quieten linter

--- internal/cmd/precache/main.go
+++ internal/cmd/precache/main.go
@@ -11,7 +11,7 @@ import (
 
 func main2() error {
 	if len(os.Args) != 5 || os.Args[1] != "-o" {
-		return fmt.Errorf("usage: %s -o dest words dict:dict:...", path.Base(os.Args[0]))
+		return fmt.Errorf("usage: %s -o dest words dict:dict:... ", path.Base(os.Args[0]))
 	}
 	dest := os.Args[2]
 	wordsPath := os.Args[3]
