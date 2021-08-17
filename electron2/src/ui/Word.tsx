113620c76d18a71193d42d81c5312203b2a5d1c4
Marcelo Cantos <marcelo.cantos@gmail.com>
2021-08-17 16:49:45 +1000

    tweak definitions ui

--- electron2/src/ui/Word.tsx
+++ electron2/src/ui/Word.tsx
@@ -17,7 +17,9 @@ export default function 汉字({字, 分数, 定义}: Props): JSX.Element {
       trigger="click"
       placement="top"
       overlay={props =>
-        <Popover className="定义弹出框" {...props} id={`汉字-{字}`}>{定义}</Popover>
+        <Popover className="定义弹出框" {...props} id={`汉字-{字}`}>
+          <Popover.Body>{定义}</Popover.Body>
+        </Popover>
       }
     >
       <span className="字">{字}<分数条 分数={分数}/></span>
