31b0ef3a2910270be2dd26d73640f2c65163d277
Marcelo Cantos <marcelo.cantos@gmail.com>
2022-08-13 15:54:15 +1000

    minor ui fix

--- electron/src/ui/Results.tsx
+++ electron/src/ui/Results.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import 汇报类 from '../engine/Report';
+import Word from '../pinyin/Word';
 import Alts from '../pinyin/Alts';
 
 import { 条目清单 } from './Decorate';
@@ -23,19 +24,27 @@ export default function 结果清单({汇报}: Props): JSX.Element {
       {结果组组.map((结果组, i) =>
         <div key={i} style={{display: "flex", flexWrap: "wrap"}}>
           {结果组[0].不及格
-            ? 结果组.map((结果, i) =>
-                <React.Fragment key={i}>
-                  ❌&nbsp;&nbsp;
-                  <Alts.HTML alts={结果.Answer}/>
-                  &nbsp; ≠ &nbsp;
-                  <汉字
-                    className="bad"
-                    字={结果.Word}
-                    分数={结果.Score}
-                    定义={<条目清单 清单={结果.Entries}/>}
-                  />
-                </React.Fragment>
-              )
+            ? 结果组.map((结果, i) => {
+                let entry: JSX.Element;
+                try {
+                  entry = Word.HTML({word: 结果.Answer});
+                } catch {
+                  entry = Alts.HTML({alts: 结果.Answer});
+                }
+                return (
+                  <React.Fragment key={i}>
+                    ❌&nbsp;&nbsp;
+                    {entry}
+                    &nbsp; ≠ &nbsp;
+                    <汉字
+                      className="bad"
+                      字={结果.Word}
+                      分数={结果.Score}
+                      定义={<条目清单 清单={结果.Entries}/>}
+                    />
+                  </React.Fragment>
+                );
+              })
             : 结果组.map((结果, i) =>
                 <React.Fragment key={i}>
                   <汉字
